8022713e6e8afdcba6863a995769ac70181b501e
tracer-ysyx <tracer@ysyx.org>
2025-07-01 23:35:12 +0800

    > compile NEMU ysyx_null 李瑞祥 Linux TerraPrisma 6.6.87.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Apr 21 17:08:54 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:35:12 up  1:37,  1 user,  load average: 0.06, 0.01, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -85,7 +85,7 @@ static int cmd_x(char *args){
   char *arg_len = strtok(NULL, " ");
   char *arg_addr = strtok(NULL, " ");
   int len = (int) arg_len[0];
-  vaddr_t addr = (vaddr_t) arg_addr[0];
+  vaddr_t addr = strtol(arg_addr,NULL,16);
   word_t word = vaddr_read(addr,len);
   printf("%d",word);
   return 0;
