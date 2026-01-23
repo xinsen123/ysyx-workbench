39b53c74cb6392d21f167637392dab4319c3ad3d
tracer-ysyx <tracer@ysyx.org>
2026-01-23 11:16:05 +0800

    > sim RTL ysyx_null 李瑞祥 Linux Thanatos 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:16:05 up  1:35,  1 user,  load average: 0.13, 0.07, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,27 @@
+#include <assert.h>
+#include <cstddef>
+#include <ctime>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "obj_dir/Vtop.h"
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+    Vtop *top = new Vtop;
+
+    time_t last = 0;
+
+    while (1) {
+        if (time(NULL) - last) {
+            last = time(NULL);
+            int a = rand() & 1;
+            int b = rand() & 1;
+            top->a = a;
+            top->b = b;
+            top->eval();
+            printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+            assert(top->f == (a ^ b));
+        }
+    }
 }
