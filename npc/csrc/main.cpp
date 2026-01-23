acb8cb64252571a0445050f6a0524927e02cfd96
tracer-ysyx <tracer@ysyx.org>
2026-01-23 14:38:22 +0800

    > sim RTL ysyx_null 李瑞祥 Linux Thanatos 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:38:22 up  4:57,  1 user,  load average: 0.01, 0.02, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -6,11 +6,26 @@
 #include <time.h>
 
 #include "../obj_dir/Vtop.h"
+#include "verilated.h"
+
+VerilatedContext *contextp = new VerilatedContext; // 用于保存仿真的时间
+
+#if CONFIG_FST_WAVE_TRACE               // 如果--trace 生成波形文件
+#include "verilated_fst_c.h"            //包含头文件
+VerilatedFstC *tfp = new VerilatedFstC; // 创建一个波形文件指针
+#endif
 
 int main() {
     Vtop *top = new Vtop;
 
     time_t last = 0;
+
+#if CONFIG_FST_WAVE_TRACE
+    contextp->traceEverOn(true); // 启用跟踪
+    top->trace(tfp, 99);         // 采样深度为99
+    tfp->open(
+        "../wave.fsp"); // 打开波形文件，文件地址和文件名可以自定义
+#endif
 
     while (1) {
         if (time(NULL) - last) {
