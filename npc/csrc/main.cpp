cc38e500d5ea92bd844c1ff4f1bebc6a286cf2e0
none-Li RuiXiang <3074906766@qq.com>
2026-01-23 15:05:17 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard 900991bf0fde6284bad629d2ff66777b6eaa41f9 Merge pull request #49 from SArthurX/fix-render-driver

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,55 @@
+#include <assert.h>
+#include <cstddef>
+#include <ctime>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../obj_dir/Vtop.h"
+#include "verilated.h"
+
+#define CONFIG_FST_WAVE_TRACE 1
+
+VerilatedContext *contextp = new VerilatedContext; // 用于保存仿真的时间
+
+#if CONFIG_FST_WAVE_TRACE               // 如果--trace 生成波形文件
+#include "verilated_fst_c.h"            //包含头文件
+VerilatedFstC *tfp = new VerilatedFstC; // 创建一个波形文件指针
+#endif
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+    Vtop *top = new Vtop;
+
+    time_t last = 0;
+    time_t begin = time(NULL) + 10;
+
+#if CONFIG_FST_WAVE_TRACE
+    contextp->traceEverOn(true); // 启用跟踪
+    top->trace(tfp, 99);         // 采样深度为99
+    tfp->open("wave.fst");    // 打开波形文件，文件地址和文件名可以自定义
+#endif
+
+    while (begin > time(NULL)) {
+        if (time(NULL) - last) {
+            last = time(NULL);
+            int a = rand() & 1;
+            int b = rand() & 1;
+            top->a = a;
+            top->b = b;
+            top->eval();
+            printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+            assert(top->f == (a ^ b));
+
+            contextp->timeInc(1); //推动仿真时间
+
+#if CONFIG_FST_WAVE_TRACE
+            tfp->dump(contextp->time()); // 按照时间采样
+#endif
+        }
+    }
+
+#if CONFIG_FST_WAVE_TRACE
+    tfp->close(); // 关闭波形文件
+#endif
+
 }
