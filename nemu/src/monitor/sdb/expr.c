42caa1eef1530d979c88493cd9f73cde9eed6561
tracer-ysyx <tracer@ysyx.org>
2026-02-01 20:28:28 +0800

    > compile NEMU ysyx_null 李瑞祥 Linux Thanatos 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:28:28 up  6:23,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -198,7 +198,8 @@ int eval(uint32_t p, uint32_t q) {
     } else {
         uint32_t op;
         op = main_sign(p, q, '*', '/');
-        op = main_sign(p, q, '+', '-'); // 后加减先乘除，若有加减则会覆盖
+        if (op == 0)
+            op = main_sign(p, q, '+', '-'); // 后加减先乘除，若有加减则会覆盖
         if (op == 0)
             assert(0);
 
