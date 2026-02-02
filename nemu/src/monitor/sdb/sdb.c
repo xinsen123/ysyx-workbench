b474d74fcf51efa2d2f024fe7bc424c961e92e7c
tracer-ysyx <tracer@ysyx.org>
2026-02-02 21:46:29 +0800

    > compile NEMU ysyx_null 李瑞祥 Linux Thanatos 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:46:29 up  2:41,  1 user,  load average: 0.04, 0.01, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -143,7 +143,7 @@ static int cmd_testexpr(char *args) {
         expa = strchr(buf, ' '); // 跳过第一个空格前面的内容
         infer = atoi(buf);
         result = expr(expa, &success);
-        if (infer != result) panic("Error result in calculate"); // 结果检查
+        if (infer != result) Log("Error result in calculate"); // 结果检查
         
         printf("expr: %s result: %u\n", expa, result);
         count++;
