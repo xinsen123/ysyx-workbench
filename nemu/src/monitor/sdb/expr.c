02616936e09eb0ebb6a34deb79217f6a2063484f
tracer-ysyx <tracer@ysyx.org>
2026-02-05 10:48:24 +0800

    > compile NEMU ysyx_null 李瑞祥 Linux Thanatos 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:48:24 up 56 min,  1 user,  load average: 0.04, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -193,10 +193,10 @@ int main_sign(int p, int q, int *op_list, int size) {
     for (i = q; i >= p; i--) {
         for (pos = 0; pos < size; pos++) {
             if (tokens[i].type == op_list[pos]) {
-                if (is_type(tokens[i].type, ambi_type, sizeof(ambi_type)) ==
-                    true) {
+                if (is_type(tokens[i].type, ambi_type,
+                            sizeof(ambi_type) / sizeof(int)) == true) {
                     if (i == p || is_type(tokens[i - 1].type, oppo_f_type,
-                                          sizeof(oppo_f_type))) {
+                                          sizeof(oppo_f_type) / sizeof(int))) {
                         continue;
                     }
                 }
@@ -217,11 +217,12 @@ int main_sign(int p, int q, int *op_list, int size) {
 
 static int eval_op_eval(uint32_t p, uint32_t q) {
     uint32_t op;
-    op = main_sign(p, q, op_5th, sizeof(op_5th)); // 运算具有优先级顺序
-    if (op == 0) op = main_sign(p, q, op_4th, sizeof(op_4th));
-    if (op == 0) op = main_sign(p, q, op_3th, sizeof(op_3th));
-    if (op == 0) op = main_sign(p, q, op_2th, sizeof(op_2th));
-    if (op == 0) op = main_sign(p, q, op_1th, sizeof(op_1th));
+    op = main_sign(p, q, op_5th,
+                   sizeof(op_5th) / sizeof(int)); // 运算具有优先级顺序
+    if (op == 0) op = main_sign(p, q, op_4th, sizeof(op_4th) / sizeof(int));
+    if (op == 0) op = main_sign(p, q, op_3th, sizeof(op_3th) / sizeof(int));
+    if (op == 0) op = main_sign(p, q, op_2th, sizeof(op_2th) / sizeof(int));
+    if (op == 0) op = main_sign(p, q, op_1th, sizeof(op_1th) / sizeof(int));
 
     Assert(op != 0, "Invalid expr");
 
@@ -267,7 +268,8 @@ int eval(uint32_t p, uint32_t q) {
          * If that is the case, just throw away the parentheses.
          */
         return eval(p + 1, q - 1);
-    } else if (is_type(tokens[p].type, ambi_type, sizeof(ambi_type))) {
+    } else if (is_type(tokens[p].type, ambi_type,
+                       sizeof(ambi_type) / sizeof(int))) {
         if (q - p == 1 || check_parentheses(p + 1, q)) {
             return -eval(p + 1, q);
         }
