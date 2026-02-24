3a4e13cc5a63426a0bfdd6adddc0f7e4750e2753
tracer-ysyx <tracer@ysyx.org>
2026-02-24 21:23:36 +0800

    > compile NEMU ysyx_null 李瑞祥 Linux Thanatos 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:23:36 up 11:44,  1 user,  load average: 0.21, 0.13, 0.05

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,28 +1,64 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static void itoa(int num, char *dst, int *count) {
+    if (num != 0)
+        itoa(num / 10, dst + 1, count);
+    *dst = (num % 10) + '0';
+    count++;
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+    panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+    // panic("Not implemented");
+    va_list args;
+    va_start(args, fmt);
+    int count = 0;
+    char *str;
+
+    const char *buf = fmt;
+    while (*buf != '\0') {
+        if (*buf == '%') {
+            switch (*buf + 1) {
+            case 'd':
+                itoa(va_arg(args, int), out, &count);
+                break;
+            case 's':
+                str = va_arg(args, char *);
+                while (*str != '\0') {
+                    *(out + count) = *str;
+                    count++;
+                    str++;
+                }
+                break;
+            }
+            buf++;
+        } else {
+            *(out + count) = *buf;
+            count++;
+        }
+        buf++;
+    }
+    return count;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+    panic("Not implemented");
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    panic("Not implemented");
 }
 
 #endif
