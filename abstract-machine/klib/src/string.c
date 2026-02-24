3ec87aacb780a57473d21d7df81f40f296345d5b
tracer-ysyx <tracer@ysyx.org>
2026-02-24 17:48:28 +0800

    > compile NEMU ysyx_null 李瑞祥 Linux Thanatos 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:48:28 up  8:09,  1 user,  load average: 0.00, 0.01, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,14 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-    panic("Not implemented");
+    // panic("Not implemented");
+    int i = 0;
+    while (src[i] != '\0' && dst[i] != '\0') {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,7 +24,17 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-    panic("Not implemented");
+    // panic("Not implemented");
+    int i = 0;
+    const char *buf = src;
+    while(dst[i] != '\0') i++;
+    while(*buf != '\0') {
+        dst[i] = *buf;
+        i++;
+        buf++;
+    }
+    dst[i] = '\0';
+    return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
@@ -35,7 +52,13 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-    panic("Not implemented");
+    // panic("Not implemented");
+    char *buf = (char *)s;
+    int i;
+    for (i = 0; i < n; i++) {
+        buf[i] = c;
+    }
+    return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -48,6 +71,12 @@ void *memcpy(void *out, const void *in, size_t n) {
 
 int memcmp(const void *s1, const void *s2, size_t n) {
     panic("Not implemented");
+    // const char *p = (const char *)s2;
+    
+    // int i;
+    // for(i=0; i<n;i++){
+
+    // }
 }
 
 #endif
